4f6b39bf6cc096d07858ac02816d5885dc63388c
asif10h <habibullahasif71@gamil.com>
2025-10-28 23:19:48 +0600

    fixed footer icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,40 +1,40 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faFacebook,
     faInstagram,
     faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
         <footer className="bg-gray-50 border-t border-gray-200 shadow-inner">
             <div className="container mx-auto px-28 py-8 flex flex-col md:flex-row items-center justify-between">
                 <div className="flex gap-6 mb-4 md:mb-0">
-                    <a
+                    <Link
                         href="https://www.facebook.com/h.m.hady.2024"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="text-blue-600 hover:text-blue-800 transition-transform transform hover:scale-110"
                     >
                         <FontAwesomeIcon icon={faFacebook} size="2x" />
-                    </a>
-                    <a
+                    </Link>
+                    <Link
                         href="https://www.instagram.com/artoonist_hplus"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="text-pink-500 hover:text-pink-700 transition-transform transform hover:scale-110"
                     >
                         <FontAwesomeIcon icon={faInstagram} size="2x" />
-                    </a>
-                    <a
+                    </Link>
+                    <Link
                         href="https://www.youtube.com/@artoonist_hplus"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="text-red-600 hover:text-red-800 transition-transform transform hover:scale-110"
                     >
                         <FontAwesomeIcon icon={faYoutube} size="2x" />
-                    </a>
+                    </Link>
                 </div>
                 <div>
                     <p className="text-gray-600 text-center text-sm md:text-base leading-relaxed">
--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -32,12 +32,12 @@ const ScrollToTop = () => {
         <div
           onClick={scrollToTop}
           className="fixed bottom-4 md:bottom-8 right-4 md:right-8 bg-sky-600 text-white p-4 rounded-full shadow-lg cursor-pointer hover:bg-sky-700 transition-colors duration-300 z-50"
-        >
+        > 
           <FontAwesomeIcon icon={faArrowUp} size="lg" />
         </div>
       )}
     </>
-  );
+  ); 
 };
 
 export default ScrollToTop;
